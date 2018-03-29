44ae45dfeef11ce3726ebf1a95cfb56bd24f734d
Tiago Cardoso <tiago_cardoso_1@hotmail.com>
2018-03-29 15:51:21 +0100

    Web service getting v1.0

--- App/SDNApp/SDNApp/Form1.Designer.cs
+++ App/SDNApp/SDNApp/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonGetSDNData = new System.Windows.Forms.Button();
+            this.textBoxSDNData = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SDN IP";
+            // 
+            // buttonGetSDNData
+            // 
+            this.buttonGetSDNData.Location = new System.Drawing.Point(12, 44);
+            this.buttonGetSDNData.Name = "buttonGetSDNData";
+            this.buttonGetSDNData.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetSDNData.TabIndex = 2;
+            this.buttonGetSDNData.Text = "Get Data";
+            this.buttonGetSDNData.UseVisualStyleBackColor = true;
+            this.buttonGetSDNData.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSDNData
+            // 
+            this.textBoxSDNData.Location = new System.Drawing.Point(12, 18);
+            this.textBoxSDNData.Name = "textBoxSDNData";
+            this.textBoxSDNData.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSDNData.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(201, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(93, 49);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(37, 13);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(485, 267);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBoxSDNData);
+            this.Controls.Add(this.buttonGetSDNData);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGetSDNData;
+        private System.Windows.Forms.TextBox textBoxSDNData;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
 
--- App/SDNApp/SDNApp/Form1.cs
+++ App/SDNApp/SDNApp/Form1.cs
@@ -2,19 +2,115 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json.Linq;
+
 
 namespace SDNApp
 {
     public partial class Form1 : Form
     {
+        string baseURI = @"http://recurso.dad/api/users";
+        private List<User> listaUsers;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //IPAddress ipAddress;
+            //if (IPAddress.TryParse(textBoxSDNData.Text, out ipAddress))
+            //{
+            //    Debug.WriteLine("### VALID IP ###");
+
+            //}
+            //else
+            //{
+            //    Debug.WriteLine("### INVALID IP ###");
+            //}
+
+            string URI = baseURI;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
+
+            if (testConnectionWebService(URI, request))
+            {
+                this.getUsers();
+
+            }
+
+        }
+
+        // funcao para testes de connection com um webservice
+        private Boolean testConnectionWebService(string URI, HttpWebRequest request)
+        {
+            try
+            {
+                request.GetResponse();
+                Debug.WriteLine("### Acedeu ao URI: " + URI + " ###");
+                this.labelStatus.ForeColor = System.Drawing.Color.Green;
+                this.labelStatus.Text = "OK";
+                return true;
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Nao foi possivel aceder ao URI: " + URI + " ###");
+                MessageBox.Show("ERROR 1: " + URI + " - NOT AVAILABLE");
+                this.labelStatus.ForeColor = System.Drawing.Color.Red;
+                this.labelStatus.Text = "OFF";
+                return false;
+            }
+        }
+
+        // faz um pedido get a um URL
+        private string getURIcontent(string URI)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
+
+            if (testConnectionWebService(URI, request))
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        Console.WriteLine(reader.ReadToEnd());
+                        //MessageBox.Show(reader.ReadToEnd());
+
+                        JObject results = JObject.Parse(reader.ReadToEnd());
+                        foreach (var result in results["Data"])
+                        {
+
+                        }
+
+                        return reader.ReadToEnd();
+                    }
+                }
+
+            }
+            return null;
+        }
+
+        // testes
+        private void getUsers()
+        {
+            string URI = baseURI;
+            string content = getURIcontent(URI);
+
+
+        }
     }
 }
+
+
