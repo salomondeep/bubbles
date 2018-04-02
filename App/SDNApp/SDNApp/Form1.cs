cb4e683cc95c4c3d6c9d1dea94c4de68de81337c
Tiago Cardoso <tiago_cardoso_1@hotmail.com>
2018-04-02 18:29:33 +0100

    get node ids & tp ids

--- App/SDNApp/SDNApp/Form1.cs
+++ App/SDNApp/SDNApp/Form1.cs
@@ -22,6 +22,8 @@ namespace SDNApp
         private String responseString = "";
         static String username = "admin";
         static String password = "admin";
+        string ip = "";
+        private string port = "8181";
         String encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
 
         public Form1()
@@ -29,16 +31,18 @@ namespace SDNApp
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+
+        private void buttonGetNodeIDS_Click(object sender, EventArgs e)
         {
             IPAddress ipAddress;
-
             if (IPAddress.TryParse(textBoxSDNIP.Text, out ipAddress))
             {
                 //Debug.WriteLine("### VALID IP ###");
-                MessageBox.Show("### VALID IP ###");
+                //MessageBox.Show("### VALID IP ###");
 
-                string URI = baseURI + textBoxSDNIP.Text + ":8181/restconf/operational/network-topology:network-topology";
+                this.ip = textBoxSDNIP.Text;
+                string URI = baseURI + this.ip + ":" + this.port + "/restconf/operational/network-topology:network-topology";
 
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
                 request.Headers.Add("Authorization", "Basic " + encoded);
@@ -47,16 +51,13 @@ namespace SDNApp
                 {
                     this.getNodeIds(request);
                 }
-
             }
             else
             {
                 //Debug.WriteLine("### INVALID IP ###");
-                MessageBox.Show("### INVALID IP ###");
+                //MessageBox.Show("### INVALID IP ###");
             }
         }
-
-
 
         private void getNodeIds(HttpWebRequest request)
         {
@@ -68,37 +69,94 @@ namespace SDNApp
                 responseString = reader.ReadToEnd();
             }
             //MessageBox.Show(responseString);
-            parseTextTest(responseString);
 
-        }
-
-
-
-        private void parseTextTest(string text)
-        {
-            JObject networkTopology = JObject.Parse(text);
+            JObject networkTopology = JObject.Parse(responseString);
             var aux = networkTopology["network-topology"];
 
             //DEBUG
-            //MessageBox.Show(networkTopology["network-topology"].ToString());
-            MessageBox.Show(aux.ToString());
+            //MessageBox.Show(aux.ToString());
 
             foreach (var result in aux["topology"])
             {
                 string topologyID = (string)result["topology-id"];
-                MessageBox.Show(topologyID);
+                //MessageBox.Show(topologyID);
 
                 foreach (var result2 in result["node"])
                 {
                     //string tpId = (string)result2["host-tracker-service:attachment-points"];
-                    MessageBox.Show(result2.ToString());
+                    //MessageBox.Show(result2.ToString());
+                    string nodeID = (string)result2["node-id"];
+                    //MessageBox.Show("topology: " + topologyID + " | node-id: " + nodeID);
+                    listBoxNodeIDS.Items.Add(nodeID);
                 }
-
             }
-
         }
 
 
+
+
+
+        private void buttonGetNodeTerminationPoints_Click(object sender, EventArgs e)
+        {
+            listBoxNodeTerminationPoints.Items.Clear();
+
+            IPAddress ipAddress;
+            if (IPAddress.TryParse(textBoxSDNIP.Text, out ipAddress))
+            {
+                //Debug.WriteLine("### VALID IP ###");
+                //MessageBox.Show("### VALID IP ###");
+
+                this.ip = textBoxSDNIP.Text;
+                string URI = baseURI + this.ip + ":" + this.port + "/restconf/operational/network-topology:network-topology/topology/flow:1/node/" + listBoxNodeIDS.Text;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
+                request.Headers.Add("Authorization", "Basic " + encoded);
+
+                if (testConnectionWebService(URI, request))
+                {
+                    this.getTPs(request);
+                }
+            }
+            else
+            {
+                //Debug.WriteLine("### INVALID IP ###");
+                //MessageBox.Show("### INVALID IP ###");
+            }
+        }
+
+        private void getTPs(HttpWebRequest request)
+        {
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                responseString = reader.ReadToEnd();
+            }
+            //MessageBox.Show(responseString);
+
+            JObject networkTopology = JObject.Parse(responseString);
+            var aux = networkTopology["node"];
+
+            //DEBUG
+            //MessageBox.Show(aux.ToString());
+
+
+            foreach (var result in aux)
+            {
+                //MessageBox.Show((string)result["node-id"]);
+
+                foreach (var result2 in result["termination-point"])
+                {
+                    //MessageBox.Show(result2.ToString());
+
+                    string tpID = (string)result2["tp-id"];
+                    //MessageBox.Show(tpID);
+
+                    listBoxNodeTerminationPoints.Items.Add(tpID);
+                }
+            }
+        }
 
 
         // funcao para testes de connection com um webservice
@@ -120,6 +178,16 @@ namespace SDNApp
                 this.labelStatus.Text = "OFF";
                 return false;
             }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBoxSDNIP.Text = "192.168.10.3";
+        }
+
+        private void listBoxNodeIDS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            buttonGetNodeTerminationPoints.Enabled = true;
         }
     }
 }
